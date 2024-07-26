1299370a94239f13df255a4b4e5718256784d1be
kayo <kayohramos@gmail.com>
2024-07-26 20:12:48 -0300

    refactor:filter_products_delivered com lambda

--- Projeto/etl.py
+++ Projeto/etl.py
@@ -18,13 +18,19 @@ sale_items = reader_csv(path_file)
 print(sale_items)
 """
 
-
+"""
 def filter_products_delivered(list_p: list[dict]) -> list[dict]:
     list_filtered_product = []
     for product in list_p:
         if product.get('entregue') == 'True':
             list_filtered_product.append(product)
     return list_filtered_product
+"""
+
+
+#Refatorando
+def filter_products_delivered(list_p: list[dict]) -> list[dict]:
+    return list(filter(lambda product: product.get('entregue') == 'True', list_p))
 
 
 """
